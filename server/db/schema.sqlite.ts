a98ead085208ddb9c9554f345e32f6c180cf6a9f
Amar Gill <amar.s.gill@outlook.com>
2026-02-04 02:36:30 -0500

    new patients schema

--- server/db/schema.sqlite.ts
+++ server/db/schema.sqlite.ts
@@ -1,4 +1,5 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
+import { sql } from 'drizzle-orm'
 
 export const messages = sqliteTable('messages', {
   id: integer().primaryKey({ autoIncrement: true }),
@@ -11,7 +12,16 @@ export const patients = sqliteTable('patients', {
   firstName: text('first_name').notNull(),
   lastName: text('last_name').notNull(),
   middleName: text('middle_name'),
-  dateOfBirth: integer('date_of_birth').notNull(),
+  dateOfBirth: integer('date_of_birth', { mode: 'timestamp' }).notNull(),
   address: text().notNull(),
-  status: text().notNull().default('inquiry'), // todo: make this an enum
+  status: text()
+    .notNull()
+    .$type<'inquiry' | 'onboarding' | 'active' | 'churned'>()
+    .default('inquiry'),
+  createdAt: integer('created_at', { mode: 'timestamp' })
+    .notNull()
+    .default(sql`(strftime('%s','now'))`),
+  updatedAt: integer('updated_at', { mode: 'timestamp' })
+    .notNull()
+    .default(sql`(strftime('%s','now'))`),
 })
