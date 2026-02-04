83601c99074300a6bfe32e9a87ba8a52d5489688
Amar Gill <amar.s.gill@outlook.com>
2026-02-04 03:57:06 -0500

    setup home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,23 @@
 <template>
   <UDashboardPanel>
-    <div class="p-2">
-      <UPageCard> todo </UPageCard>
-    </div>
+    <template #header>
+      <UDashboardNavbar title="Dashboard" />
+    </template>
+    <template #body>
+      <UContainer>
+        <UPageCard
+          title="Welcome to the Dashboard"
+          description="Here you can manage your application."
+          icon="mdi-view-dashboard"
+          class="max-w-2xl mx-auto"
+          variant="subtle"
+        >
+          <p class="text-center">
+            Use the navigation menu to access different sections of the
+            dashboard.
+          </p>
+        </UPageCard>
+      </UContainer>
+    </template>
   </UDashboardPanel>
 </template>
